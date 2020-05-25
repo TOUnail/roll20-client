7f50e7cdf7c5b4965b86a7ca6f7ab30f017e85d2
atou <atou@marinedepot.com>
2020-05-24 19:52:47 -0700

    fix nav bar header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,17 @@ import { faDiceD20 } from "@fortawesome/pro-regular-svg-icons/faDiceD20";
 
 function Navbar() {
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-primary p-3">
+    <div className="bg-primary p-3">
       <div className="container">
-        <div className="flex items-center flex-shrink-0 text-white">
-          <FontAwesomeIcon icon={faDiceD20} size="2x" className="mr-2" />
-          <span className="font-semibold text-xl tracking-light">Logo</span>
-        </div>
+        <nav className="flex items-center justify-between flex-wrap ">
+          <div className="flex items-center flex-shrink-0 text-white">
+            <FontAwesomeIcon icon={faDiceD20} size="2x" className="mr-2" />
+            <span className="font-semibold text-xl tracking-light">Logo</span>
+          </div>
+          <div>Login/Sign Up</div>
+        </nav>
       </div>
-    </nav>
+    </div>
   );
 }
 
