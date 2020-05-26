8954c7108cee93e72b1e60426e6d96b0c10fd7db
atou <atou@marinedepot.com>
2020-05-25 21:41:44 -0700

    redesign top nav, test fetch post

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDiceD20 } from "@fortawesome/pro-regular-svg-icons/faDiceD20";
 
 function Navbar() {
   return (
-    <div className="bg-primary p-3">
+    <div className="bg-white p-2 border-gray-200 border-b sticky top-0">
       <div className="container">
         <nav className="flex items-center justify-between flex-wrap ">
-          <div className="flex items-center flex-shrink-0 text-white">
-            <FontAwesomeIcon icon={faDiceD20} size="2x" className="mr-2" />
-            <span className="font-semibold text-xl tracking-light">Logo</span>
+          <Link to={"/"} className="flex items-center flex-shrink-0">
+            <FontAwesomeIcon
+              icon={faDiceD20}
+              size="2x"
+              className="mr-2 text-primary-900"
+            />
+            <span className="font-semibold text-xl tracking-light text-gray-900">
+              Logo
+            </span>
+          </Link>
+          <div className="flex items-center text-white">
+            <Link
+              to={"/login"}
+              className="bg-gray-200 text-sm text-gray-900 py-1 px-2 rounded mr-2 font-semibold"
+            >
+              Log in
+            </Link>
+            <Link
+              to={"/signup"}
+              className="bg-primary-900 text-white text-sm font-semibold rounded py-1 px-2"
+            >
+              Sign up
+            </Link>
           </div>
-          <div>Login/Sign Up</div>
         </nav>
       </div>
     </div>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function home() {
+const Home = () => {
+  const [hasError, setErrors] = useState(false);
+  const [data, setData] = useState({});
+
+  const fetchData = async () => {
+    const posts = await fetch("/posts");
+    posts
+      .json()
+      .then((res) => setData(res))
+      .catch((err) => setErrors(err));
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="container">
+      <div className="grid grid-cols-1 md:grid-cols-3">
+        <div className="col-span-1 md:col-span-2">
+          {!hasError && JSON.stringify(data)}
+        </div>
+
+        <div className="col-span-1">.col-span-1</div>
+      </div>
     </div>
   );
-}
+};
 
-export default home;
+export default Home;
