8c842fac2e20033b4abaaa9e392100f8e34942bd
atou <atou@marinedepot.com>
2020-05-29 21:00:46 -0700

    design card for posts

---
+++ src/components/Post.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Post = (props) => {
+  console.log(props);
+  const {
+    post: {
+      body,
+      createdAt,
+      userImage,
+      userHandle,
+      postId,
+      likeCount,
+      commentCount,
+      roll,
+    },
+  } = props;
+  return (
+    <div className="w-full">
+      <div className="shadow bg-white p-4 my-2 sm:rounded-lg flex flex-col justify-between leading-normal">
+        <div className="flex">
+          <img
+            className="w-10 h-10 rounded-full mr-4"
+            src={userImage}
+            alt="user"
+          />
+          <div className="text-sm">
+            <p className="text-gray-900 leading-none">
+              <Link to={`/users/${userHandle}`}>{userHandle}</Link> rolled a{" "}
+              {roll}
+            </p>
+            <p className="text-gray-600">{createdAt}</p>
+            <p>{body}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,27 +1,34 @@
 import React, { useState, useEffect } from "react";
+import Post from "../components/Post";
 
 const Home = () => {
   const [hasError, setErrors] = useState(false);
-  const [data, setData] = useState({});
+  const [data, setData] = useState(null);
 
-  const fetchData = async () => {
-    const posts = await fetch("/posts");
-    posts
-      .json()
-      .then((res) => setData(res))
-      .catch((err) => setErrors(err));
-  };
   useEffect(() => {
+    const fetchData = async () => {
+      const posts = await fetch("/posts");
+      posts
+        .json()
+        .then((res) => setData(res))
+        .catch((err) => {
+          setErrors(err);
+          console.log(hasError);
+        });
+    };
     fetchData();
-  }, []);
+  }, [hasError]);
 
   return (
     <div className="container">
       <div className="grid grid-cols-1 md:grid-cols-3">
         <div className="col-span-1 md:col-span-2">
-          {!hasError && JSON.stringify(data)}
+          {data ? (
+            data.map((post) => <Post key={post.postId} post={post} />)
+          ) : (
+            <p>Loading...</p>
+          )}
         </div>
-
         <div className="col-span-1">.col-span-1</div>
       </div>
     </div>
